08202974204e9f771148bdaee63aa01b54660fdd
visuallyadvised <snstocking@opencad.io>
2023-07-23 02:57:49 -0500

    changes
    issuing agency was added to BOLOs; user table was added (may scrap and recreate with more info).

--- static/create_tables.sql
+++ static/create_tables.sql
@@ -1,5 +1,18 @@
 ### WARNING! This SQL file is the home of creating all database tables, please do not alter this file unless you know what you are doing!###
 
+CREATE TABLE IF NOT EXISTS `<DATA_PREFIX>userInformation` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` text NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `password` text DEFAULT NULL,
+    `privilege` int(11) COMMENT '0 MEANS NO ADDITIONAL PERMS, 1 MEANS SUPERVISORY PERMS, 2 MEANS ADMIN PERMS.',
+    `passwordReset` int(1) DEFAULT 0 COMMENT '1 means that a password reset is required before continuing, 0 means it is good',
+    `accountApproval` int(1) DEFAULT 0 COMMENT 'Account is disabled by default, account will need approval before using account. 0 is pending approval, 1 is access, 2 is SUSPENSED',
+    `suspensionComment` tinytext DEFAULT NULL COMMENT 'Give a reason for the suspension of the account!',
+    `suspensionDuration` date DEFAULT NULL COMMENT 'Duration of suspension',
+    UNIQUE KEY `id` (`id) 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
 CREATE TABLE IF NOT EXISTS `<DATA_PREFIX>configuration` (
     `hash` varchar(255) NOT NULL COMMENT 'Security Hash',
     `key` varchar(80) NOT NULL COMMENT 'Key for Option',
@@ -25,6 +38,7 @@ CREATE TABLE IF NOT EXISTS `<DATA_PREFIX>boloVehicle` (
   `secondaryColor` varchar(255) NOT NULL COMMENT 'Secondary color of BOLO vehicle.',
   `reasonWanted` varchar(255) NOT NULL COMMENT 'Reason BOLO suspect is wanted.',
   `lastLocation` varchar(255) NOT NULL COMMENT 'Last observed location of BOLO vehicle.',
+  `issuingAgency` varchar(255) NOT NULL COMMENT 'The agency of which has issued this BOLO',
   PRIMARY KEY (`vehicleID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
@@ -36,10 +50,11 @@ CREATE TABLE IF NOT EXISTS `<DATA_PREFIX>boloPerson` (
     `physicalDescription` varchar(255) NOT NULL COMMENT 'Physical description of BOLO suspect.',
     `reasonIssued` varchar(255) NOT NULL COMMENT 'Reason BOLO suspect is wanted.',
     `lastSeen` varchar(255) NOT NULL COMMENT 'Last observed location of BOLO suspect.',
+    `issuingAgency` varchar(255) NOT NULL COMMENT 'The agency of which has issued this BOLO',
   PRIMARY KEY (`id`) USING BTREE
-)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
-CREATE TABLE IF NOT EXISTS `<DATA_PREFIX>AMBERALERT` (
+CREATE TABLE IF NOT EXISTS `<DATA_PREFIX>amberAlert` (
     `alertID` int(11) NOT NULL AUTO_INCREMENT,
     `firstName` varchar(255) NOT NULL COMMENT 'This is the first name if needed in the Amber Alert.',
     `lastName` varchar(255) NOT NULL COMMENT 'This is the last name if needed in the Amber Alert.',
@@ -48,4 +63,6 @@ CREATE TABLE IF NOT EXISTS `<DATA_PREFIX>AMBERALERT` (
     `vehiclePlate` varchar(255) NOT NULL COMMENT 'License of AMBER ALERT vehicle.',
     `primaryColor` varchar(255) NOT NULL COMMENT 'Primary color of AMBER ALERT vehicle.',
     `lastSeen` varchar(255) NOT NULL COMMENT 'Last seen with... at...'
-)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+CREATE TABLE IF NOT EXISTS `<DATA_PREFIX>
